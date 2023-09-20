b91e33f2cd6d6e391cbf6973c56007170ee31fd8
fiorella frini <fiorella_frini@hotmail.com>
2023-09-20 18:41:48 -0300

    actializacion

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import axios from "axios";
 
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://pidogsback-production-008e.up.railway.app/';
+axios.defaults.baseURL = 'https://proyectodogs-production.up.railway.app/';
 
 
 ReactDOM.render(
